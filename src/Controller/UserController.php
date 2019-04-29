eae87d323514aeee2583335f60d10261cc60cc73
Lambert <alexandre.l.lambert@hotmail.com>
2019-04-29 14:25:40 +0200

    custom porduct card

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,10 +9,31 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 
 class UserController extends AbstractController
 {
+    private $encoders ;
+    private $normalizers;
+    private $serializer;
+    public function __construct()
+    {   
+        $this->encoder = new JsonEncoder();
+
+        $this->normalizer = new ObjectNormalizer();
+        $this->normalizer->setIgnoredAttributes(array('password', 'histories', 'wikies', 'documents', 'files', 'articles', 'salt', 'credentialsNonExpired', 'events'));
+        $this->normalizer->setCircularReferenceHandler(function ($object, string $format = null, array $context = array()) {
+            return $object->getId();
+        });
+        $this->serializer = new Serializer(array($this->normalizer), array($this->encoder));
+       
+    }
     
     /**
      * @Route("/register/user", methods={"POST"}, name="register_user")
@@ -49,5 +70,17 @@ class UserController extends AbstractController
     return new Response('false');
 
         }
+        
+    /**
+     * @Route("/get/users", methods={"GET"}, name="get_users")
+     */
+    public function getProducts(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $users = $this->getDoctrine()
+        ->getRepository(User::class)
+        ->findAll();
+        $jsonContent = $this->serializer->serialize($users, 'json');
+        return new Response($jsonContent);
+    }
     
 }
