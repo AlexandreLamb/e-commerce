2a00734d404d5c5d06eec7d7f556704e30bfe497
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2019-04-29 16:40:06 +0200

    login system

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -36,7 +36,6 @@ class UserController extends AbstractController
      * @Route("/check/user/mail", methods={"POST"}, name="check_user_mail")
      */
         public function checkUserMail(Request $request){
-            $repository = $this->getDoctrine()->getRepository(User::class);
             $data = json_decode($request->getContent(),true);
             $user = $this->getDoctrine()
             ->getRepository(User::class)
@@ -49,5 +48,20 @@ class UserController extends AbstractController
     return new Response('false');
 
         }
-    
+        /**
+         * @Route("/check/user", methods={"POST"}, name="check_user")
+         */
+    public function checkUser (Request $request) {
+        $data = json_decode($request->getContent(),true);
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(['email' => $data['email']]);
+            if (!$user){
+                return new Response('false');
+            } else {
+                if (  password_verify($data['password'],$user->getPassword() ) ) {
+                    return new response('true');
+                }   
+            }
+    }
 }
