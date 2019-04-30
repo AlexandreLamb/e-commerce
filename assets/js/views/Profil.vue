60ce96d424097f1b4a4939fbd9fc0eb1550fa1eb
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2019-04-30 13:13:13 +0200

    profile

---
+++ assets/js/views/Profil.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <b-jumbotron>
+    <template slot="header">Mon Profil</template>
+
+    <template slot="lead">
+      This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+      featured content or information.
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+      It uses utility classes for typography and spacing to space content out within the larger
+      container.
+    </p>
+
+    <b-button variant="primary" href="#">Mes commandes</b-button>
+    <b-button variant="success" href="#">Devnir Vendeur</b-button>
+  </b-jumbotron>
+</div>
+</template>
